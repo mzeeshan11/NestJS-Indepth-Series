4cb40a9241b3b0a260d51b1c6cecf0f7b78e7200
ZeeShan <m.zeeshan1600@gmail.com>
2022-11-06 17:31:22 +0500

    Controlling Req-Res and Post Req(DTO):

--- src/customers/controllers/customers/customers.controller.ts
+++ src/customers/controllers/customers/customers.controller.ts
@@ -1,12 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Param, ParseIntPipe, Post, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { CreateCustomerDto } from 'src/customers/dtos/CreateCustomer.dto';
 import { CustomersService } from 'src/customers/services/customers/customers.service';
 
 @Controller('customers')
 export class CustomersController {
     constructor(private customersService: CustomersService) { }
 
-    @Get('')
-    getCustomers() {
-        return this.customersService.findCustomer()
+    @Get(':id')
+    getCustomer(
+        @Param('id', ParseIntPipe) id: number,
+        @Req() req: Request,
+        @Res() res: Response,
+    ) {
+        console.log(typeof id);
+        const customer = this.customersService.findCustomerById(id)
+        if (customer) {
+            res.send(customer);
+        } else {
+            res.status(400).send({ msg: 'Customer Not Found' })
+        }
     }
+    @Get('/search/:id')
+    searchCustomerById(
+        @Param('id', ParseIntPipe) id: number
+    ) {
+        const customer = this.customersService.findCustomerById(id)
+        if (customer) return customer
+        else throw new HttpException('Customer Not Founc', HttpStatus.BAD_REQUEST)
+    }
+
+    @Get('')
+    getAllCustomers() {
+        return this.customersService.getCustomers();
+    }
+    @Post('create')
+    createCustomer(@Body() createCustomerDto: CreateCustomerDto) {
+        console.log(createCustomerDto)
+        this.customersService.createCustomer(createCustomerDto)
+    }
+
 }
--- src/customers/services/customers/customers.service.ts
+++ src/customers/services/customers/customers.service.ts
@@ -1,12 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { CreateCustomerDto } from 'src/customers/dtos/CreateCustomer.dto';
 
 @Injectable()
 export class CustomersService {
-    findCustomer() {
-        return {
+    private customers = [
+        {
             id: 1,
             email: "zee@gmail.com",
             name: "Mr.Zeeshan",
+        },
+        {
+            id: 2,
+            email: "zee@gmail.com",
+            name: "Mr.Zeeshan",
+        },
+        {
+            id: 3,
+            email: "zee@gmail.com",
+            name: "Mr.Zeeshan",
         }
+    ]
+    findCustomerById(id: number) {
+        return this.customers.find((user) => user.id === id)
+    }
+    getCustomers() {
+        return this.customers;
+    }
+    createCustomer(customerDto: CreateCustomerDto) {
+        this.customers.push(customerDto)
     }
 }
